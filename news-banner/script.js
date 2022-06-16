f5e0fdac11f2e88845c653636d0d348279f851cb
axellecousinou <axelle.cous99@gmail.com>
2022-06-16 14:42:17 +0100

    Adding news banner template with adaptations but no readme

---
+++ news-banner/script.js
@@ -0,0 +1,5 @@
+console.log(document.getElementById("newest-posts-list"))
+
+var firstElementCloned = document.getElementById("newest-posts-list").getElementsByTagName("li")[0].cloneNode(true);
+var elementsList = document.getElementById("newest-posts-list");
+elementsList.appendChild(firstElementCloned);
