2c96274b880dd61dfccc2801437dc7e5e3d7f213
adamchevalier <adamchevalier@free.fr>
2022-09-06 20:48:00 +0100

    Added template frameDivided

---
+++ frameDivided/script.js
@@ -0,0 +1,6 @@
+function changeFrameClass() {
+    document.getElementById("framePopup").classList.toggle("popup")
+    document.getElementById("framePopup").classList.toggle("popup-active")
+    document.getElementById("frameText").classList.toggle("addOverlayElementBefore")
+    document.getElementById("frameText").classList.toggle("addOverlayElementAfter")
+}
