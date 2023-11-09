b767eb00ddf1584b080a350265cd2e5bed4939fa
adamchevalier <adamchevalier@free.fr>
2023-11-09 17:00:32 +0100

    division into chunk

--- mosaic/script.js
+++ mosaic/script.js
@@ -4,7 +4,7 @@ const height = 500
 const width = 500
 const div = 25
 
-const randomOffset = 25
+const randomOffsetPercent = 100
 
 const colorList = ["#BAA963", "#B78C58", "#AF9B53", "#997C35"]
 
@@ -15,6 +15,10 @@ const defaultScale = 0.85
 const thresholdScale = 1
 const maxScale = 1.2
 
+const chunkXNum = Math.floor(width/(hoverRadius*2))
+const chunkYNum = Math.floor(height/(hoverRadius*2))
+
+
 // create points
 let pointList = []
 for (let i = 0; i <=width/div; i++) {
@@ -22,8 +26,8 @@ for (let i = 0; i <=width/div; i++) {
     for (let j = 0; j <=height/div; j++) {
         let borderX = i == 0 || i == width/div ? 0 : 1
         let borderY = j == 0 || j == height/div ? 0 : 1
-        x = i*div + Math.round(Math.random()*randomOffset*borderX)
-        y = j*div + Math.round(Math.random()*randomOffset*borderY)
+        x = i*div + Math.round(Math.random()*(randomOffsetPercent/100*div)*borderX)
+        y = j*div + Math.round(Math.random()*(randomOffsetPercent/100*div)*borderY)
 
         newLine.push([x,y])
     }
@@ -44,18 +48,50 @@ for (let i = 0; i <pointList.length; i++) {
 }
 
 
-// display pebble
+// create chunks 
+chunkList = []
+for (let i=0; i<chunkXNum; i++) {
+    newChunkList = []
+    for (let j=0; j<chunkYNum; j++) {
+        newChunk = []
+        newChunkList.push(newChunk)
+    }
+    chunkList.push(newChunkList)
+}
+
+// display chunks
+const chunkWidth = width/chunkXNum
+const chunkHeight = height/chunkYNum
+
+for (let i=0; i<chunkXNum; i++) {
+    for (let j=0; j<chunkYNum; j++) {
+        let newChunk = document.createElement("div")
+        newChunk.className = "chunk"
+        newChunk.style.width = chunkWidth + "px"
+        newChunk.style.height = chunkHeight + "px"
+        newChunk.style.left = i*chunkWidth + "px"
+        newChunk.style.top = j*chunkHeight + "px"
+        newChunk.style.zIndex = 10
+        container1.appendChild(newChunk)
+
+    }
+}
+
+
+// create pebble
 let pebbleList = []
 for (let i = 0; i < pointList.length-1; i++) {
     for (let j = 0; j<pointList[0].length-1; j++) {
         let newPebbleCoos = [pointList[i][j],pointList[i][j+1],pointList[i+1][j],pointList[i+1][j+1]]
-        let newPebble = this.createPolygon(newPebbleCoos[0][0],newPebbleCoos[0][1],newPebbleCoos[1][0],newPebbleCoos[1][1],
-            newPebbleCoos[2][0],newPebbleCoos[2][1],newPebbleCoos[3][0],newPebbleCoos[3][1])
-        container1.appendChild(newPebble)
 
         let newPebble2 = this.createPolygon2(newPebbleCoos[0][0],newPebbleCoos[0][1],newPebbleCoos[1][0],newPebbleCoos[1][1],
             newPebbleCoos[2][0],newPebbleCoos[2][1],newPebbleCoos[3][0],newPebbleCoos[3][1])
         container2.appendChild(newPebble2)
+
+        // old pebble just for understanding
+        let newPebble = this.createPolygon(newPebbleCoos[0][0],newPebbleCoos[0][1],newPebbleCoos[1][0],newPebbleCoos[1][1],
+            newPebbleCoos[2][0],newPebbleCoos[2][1],newPebbleCoos[3][0],newPebbleCoos[3][1])
+        container1.appendChild(newPebble)
     }
 }
 
@@ -71,21 +107,39 @@ document.addEventListener("mousemove", (event) => {
     hoverCircle.style.left = event.clientX + "px"
     hoverCircle.style.top = event.clientY + "px"
 
-    for (let pebble of pebbleList) {
-        const distance = this.distance(pebble.center[0],pebble.center[1],event.clientX - boundingRectX, event.clientY - boundingRectY)
-        if (distance < hoverRadius) {
-            pebble.object.style.transition = "transform 0.15s ease"
-            if (distance < minRadiusThreshold) {
-                pebble.object.style.transform = "scale("+maxScale+")"
+    const chunkX = Math.floor((event.clientX - boundingRectX)/chunkWidth)
+    const chunkY = Math.floor((event.clientY - boundingRectY)/chunkHeight)
+    
+    if (chunkX >= 0 && chunkX < chunkXNum && chunkY >= 0 && chunkY < chunkYNum) { 
+        // Loop over pebble in chunk
+        for (let pebble of chunkList[chunkX][chunkY]) {
+            const distance = this.distance(pebble.center[0],pebble.center[1],event.clientX - boundingRectX, event.clientY - boundingRectY)
+            if (distance < hoverRadius) {
+                pebble.object.style.transition = "transform 0.15s ease"
+                if (distance < minRadiusThreshold) {
+                    pebble.object.style.transform = "scale("+maxScale+")"
+                } else {
+                    const newScale = thresholdScale + (thresholdScale-defaultScale)/(minRadiusThreshold-hoverRadius)*(distance-minRadiusThreshold)
+                    pebble.object.style.transform = "scale("+newScale+")"
+                }
             } else {
-                const newScale = thresholdScale + (thresholdScale-defaultScale)/(minRadiusThreshold-hoverRadius)*(distance-minRadiusThreshold)
-                pebble.object.style.transform = "scale("+newScale+")"
+                pebble.object.style.transition = "transform 2s ease"
+                pebble.object.style.transform = "scale("+defaultScale+")"
             }
-        } else {
-            pebble.object.style.transition = "transform 2s ease"
-            pebble.object.style.transform = "scale("+defaultScale+")"
         }
     }
+
+    for (let i=0; i<chunkXNum; i++) {
+        for (let j=0; j<chunkYNum; j++) {
+            if (i != chunkX && j != chunkY) {
+                for (let pebble of chunkList[i][j]) {
+                    pebble.object.style.transition = "transform 2s ease"
+                    pebble.object.style.transform = "scale("+defaultScale+")"
+                }
+            }
+        }
+    }
+    
 });
 
 
@@ -93,13 +147,13 @@ function createPolygon2(x1, y1, x2, y2, x3, y3, x4, y4) {
     let newPebble = document.createElement("div")
     newPebble.className = "pebble"
 
-    minX = Math.min(x1,x2)
-    minY = Math.min(y1,y3) 
-    maxX = Math.max(x3,x4)
-    maxY = Math.max(y2,y4)
+    const minX = Math.min(x1,x2)
+    const minY = Math.min(y1,y3) 
+    const maxX = Math.max(x3,x4)
+    const maxY = Math.max(y2,y4)
 
-    pebbleWidth = maxX - minX
-    pebbleHeight = maxY - minY
+    const pebbleWidth = maxX - minX
+    const pebbleHeight = maxY - minY
 
     newPebble.style.width = pebbleWidth + "px"
     newPebble.style.height = pebbleHeight + "px"
@@ -111,10 +165,21 @@ function createPolygon2(x1, y1, x2, y2, x3, y3, x4, y4) {
     newPebble.style.backgroundColor = colorList[Math.floor(Math.random()*colorList.length)]
 
 
+    // full pebble List
     pebbleList.push({
         center: [minX + pebbleWidth/2, minY + pebbleHeight/2],
         object: newPebble
     })
+
+
+    // populate chunkList with pebble
+    const chunkX = Math.floor((minX + pebbleWidth/2)/chunkWidth)
+    const chunkY = Math.floor((minY + pebbleHeight/2)/chunkHeight)
+    chunkList[chunkX][chunkY].push({
+        center: [minX + pebbleWidth/2, minY + pebbleHeight/2],
+        object: newPebble
+    })
+
 
     return newPebble
 }
